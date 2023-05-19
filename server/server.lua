52f1864f5b9952879951812e722a5740dc08828c
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-05-19 21:12:05 +0400

    used ESX getName function to get ingame playername

--- server/server.lua
+++ server/server.lua
@@ -1,7 +1,11 @@
+ESX = exports["es_extended"]:getSharedObject()
+
 RegisterServerEvent('SY_Killfeed:onPlayerDead')
 AddEventHandler('SY_Killfeed:onPlayerDead', function(data)
-    data.victim = GetPlayerName(source)
-    data.killerServerId = GetPlayerName(data.killerServerId)
+    local victim = ESX.GetPlayerFromId(source)
+    local killer = ESX.GetPlayerFromId(data.killerServerId)
+    data.victim = victim.getName()
+    data.killerServerId = killer.getName()
 
     if data.killedByPlayer then
         TriggerClientEvent('SY_Killfeed:ShowUi', -1, data)
